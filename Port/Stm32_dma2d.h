fcd83f02d8cd2130ca59c763f8017e28f82bc883
BSP Release <release@bsp>
2026-09-05 18:59:55 +0000

    Release v1.6.0 for STM32H5

---
+++ Port/Stm32_dma2d.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_dma2d.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) DMA2D peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_DMA2D_H
+#define PORT_STM32_DMA2D_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32h5xx_ll_dma2d.h"              /* DMA2D peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_DMA2D_H */
