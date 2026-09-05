5bebd041a56b5b555d7f6b39b833b7fa39a08a9f
BSP Release <release@bsp>
2026-09-05 19:02:00 +0000

    Release v1.0.0 for STM32U5

---
+++ Port/Stm32_icache.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_icache.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) ICACHE peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_ICACHE_H
+#define PORT_STM32_ICACHE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32u5xx_ll_icache.h"              /* ICACHE peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_ICACHE_H */
---
+++ Port/Stm32_ucpd.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_ucpd.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) UCPD peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_UCPD_H
+#define PORT_STM32_UCPD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32u5xx_ll_ucpd.h"              /* UCPD peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_UCPD_H */
