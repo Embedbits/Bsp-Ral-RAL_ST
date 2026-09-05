833a2e8e99c7147b51d7d4b985dc30185924c84b
BSP Release <release@bsp>
2026-09-05 19:01:12 +0000

    Release v1.10.0 for STM32L4

---
+++ Port/Stm32.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_stm32l4xx.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) STM32L4XX peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_STM32L4XX_H
+#define PORT_STM32_STM32L4XX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32l4xx.h"              /* STM32L4XX peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_STM32L4XX_H */
---
+++ Port/Stm32_usart.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_usart.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) USART peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_USART_H
+#define PORT_STM32_USART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32l4xx_ll_usart.h"              /* USART peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_USART_H */
