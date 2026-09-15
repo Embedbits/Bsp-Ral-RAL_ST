fddad602a1d955321ff944a4b7d31e683d910456
BSP Release <release@bsp>
2026-09-15 23:45:08 +0000

    Release v1.10.0 for STM32L4

---
+++ Port/Stm32_lptim.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_lptim.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) LPTIM peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_LPTIM_H
+#define PORT_STM32_LPTIM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32l4xx_ll_lptim.h"              /* LPTIM peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_LPTIM_H */
