594c61169860b5b9269aceae9c0a6920a9cb9581
BSP Release <release@bsp>
2026-09-15 23:38:28 +0000

    Release v1.0.0 for STM32G4

---
+++ Port/Stm32_fmac.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_fmac.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) FMAC peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_FMAC_H
+#define PORT_STM32_FMAC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32g4xx_ll_fmac.h"              /* FMAC peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_FMAC_H */
