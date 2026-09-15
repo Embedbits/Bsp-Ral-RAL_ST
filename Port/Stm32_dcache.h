5fe3379b9cb33977b44cf64b56c0f84b7f406ba1
BSP Release <release@bsp>
2026-09-15 23:47:05 +0000

    Release v1.0.0 for STM32U5

---
+++ Port/Stm32_dcache.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_dcache.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) DCACHE peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_DCACHE_H
+#define PORT_STM32_DCACHE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32u5xx_ll_dcache.h"              /* DCACHE peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_DCACHE_H */
---
+++ Port/Stm32_gpio.h
@@ -0,0 +1,32 @@
+/**
+ * \author 
+ * \file Stm32_gpio.h
+ * \ingroup Port
+ * \brief Port file for using Register Abstraction Layer (RAL) GPIO peripheral.
+ *
+ */
+
+#ifndef PORT_STM32_GPIO_H
+#define PORT_STM32_GPIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "stm32u5xx_ll_gpio.h"              /* GPIO peripheral access layer   */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORT_STM32_GPIO_H */
